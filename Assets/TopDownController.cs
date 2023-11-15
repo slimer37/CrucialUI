3e999cdb2901461dc260a958f9259d458968b5a9
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2023-11-14 19:22:11 -0800

    Animation code

--- Assets/TopDownController.cs
+++ Assets/TopDownController.cs
@@ -1,14 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class TopDownController : MonoBehaviour
 {
     [SerializeField] float _speed = 1;
+    [SerializeField] Animator _animator;
+    [SerializeField] SpriteRenderer _sprite;
+    [SerializeField] float _crossTime;
+    [SerializeField] float _minWalkAnimSpeed;
 
     Vector2 _input;
     Rigidbody2D _rb;
+
+    Vector2 _direction;
+
+    string _queuedState;
 
     void Awake()
     {
@@ -19,6 +25,51 @@ public class TopDownController : MonoBehaviour
     {
         _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         _input = Vector3.ClampMagnitude(_input, 1);
+
+        if (_input.sqrMagnitude > 0)
+        {
+            _direction = _input;
+        }
+
+        HandleAnimation();
+    }
+
+    void HandleAnimation()
+    {
+        if (_input.x != 0)
+        {
+            _sprite.flipX = _input.x > 0;
+        }
+
+        string state;
+
+        if (Mathf.Abs(_direction.x) > Mathf.Abs(_direction.y))
+        {
+            state = "side_";
+        }
+        else if (_direction.y > 0)
+        {
+            state = "up_";
+        }
+        else
+        {
+            state = "down_";
+        }
+
+        if (_input.magnitude > _minWalkAnimSpeed)
+        {
+            state += "walk";
+        }
+        else
+        {
+            state += "idle";
+        }
+
+        if (_queuedState != state)
+        {
+            _animator.CrossFadeInFixedTime(state, _crossTime);
+            _queuedState = state;
+        }
     }
 
     void FixedUpdate()
